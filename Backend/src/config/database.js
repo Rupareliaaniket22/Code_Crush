26957e18774837daf47dc36ecdb46eca78293323
Aniket <rupareliaaniket7@gmail.com>
2025-04-18 18:00:35 +0530

    updated first version

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -2,7 +2,7 @@ const { mongoose } = require("mongoose");
 require("dotenv").config();
 
 async function databaseConnect() {
-  const connect = mongoose.connect(process.env.MONGO_URI, {
+  const connect = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
