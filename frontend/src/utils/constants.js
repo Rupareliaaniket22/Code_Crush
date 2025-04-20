c413aa01c75041fe4164d01654d33e5b2278cd06
Aniket <rupareliaaniket7@gmail.com>
2025-04-20 16:23:32 +0530

    updated constants

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "/api";
 export const DEFAULT_PHOTO_URL =
   "https://thumbs.dreamstime.com/b/default-profile-picture-avatar-photo-placeholder-vector-illustration-default-profile-picture-avatar-photo-placeholder-vector-189495158.jpg";
