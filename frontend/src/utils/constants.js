26e29b9977f968779c2c4303471e125d3aac3d8c
Aniket <rupareliaaniket7@gmail.com>
2025-04-21 15:07:36 +0530

    Made Base URl Dynamic

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,4 @@
-export const BASE_URL = "/api";
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:3000" : "/api";
 export const DEFAULT_PHOTO_URL =
   "https://thumbs.dreamstime.com/b/default-profile-picture-avatar-photo-placeholder-vector-illustration-default-profile-picture-avatar-photo-placeholder-vector-189495158.jpg";
