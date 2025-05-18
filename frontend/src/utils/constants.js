456dd34afe40c6c1e7580f6d96a83d76411463e5
Aniket Ruparelia <98379579+Rupareliaaniket22@users.noreply.github.com>
2025-05-18 16:19:20 +0530

    Update constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,6 @@
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:3000" : "/api";
+  location.hostname === "localhost"
+    ? "http://localhost:3000"
+    : "https://codecrush-production.up.railway.app";
 export const DEFAULT_PHOTO_URL =
   "https://thumbs.dreamstime.com/b/default-profile-picture-avatar-photo-placeholder-vector-illustration-default-profile-picture-avatar-photo-placeholder-vector-189495158.jpg";
